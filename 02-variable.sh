91562271714b81405efadd07a76ad2aa8062449a
mohammaddba91 <mohammaddba91@gmail.com>
2023-12-18 12:06:13 +0530

    02script changed

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Ramesh:: Hello suresh, Good Morning"
-echo "Suresh:: Hi Ramesh, very Good Morning"
-echo "Ramesh:: How are you Suresh?"
-echo "Suresh:: I am good Ramesh, How are you" 
+echo "Ramesh: Hello suresh, Good Morning"
+echo "Suresh: Hi Ramesh, very Good Morning"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: I am good Ramesh, How are you" 
