80274a954886eee2db80fa8b75ac29a335a89683
mohammaddba91 <mohammaddba91@gmail.com>
2023-12-18 13:18:24 +0530

    add new one04

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON02, Good Morning"
+echo "$PERSON02: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON02?"
+echo "$PERSON02: I am good $PERSON1, How are you" 
