3c5f609e7f36a05707698eeb8b2b9b7bd51438fd
mohammaddba91 <mohammaddba91@gmail.com>
2023-12-18 12:48:16 +0530

    changed at 03

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "rizwan:: Hello shareef,what are you doing"
-echo "shareef:: i am preparing devops"
-echo "rizwan:: tech me for me also"
-echo "shareef:: ok i will teach you"
+DATE=$(date)
+
+echo "date and time is:${DATE}
